835530d59efa5e53daf2058f24ea7787cf8ad50f
Julian Armour <julian.armour@videotron.ca>
2018-11-23 10:45:53 -0500

    Made divider look nicer and take up less space

--- src/components/fleet-status-overview/Fleet.vue
+++ src/components/fleet-status-overview/Fleet.vue
@@ -7,11 +7,12 @@
         <div class="hidden content">
           <i class="left arrow icon"></i>
         </div>
-      </div></b-row>
+      </div>
+    </b-row>
     <b-row>
       <b-col>
         <b-row>
-          <h2 class="mx-auto" style="width: 180px;"><i class="map icon"></i> Active Trips</h2>
+          <h2 class="mx-auto" style="width: 166px;"><i class="map icon"></i> Active Trips</h2>
         </b-row>
         <b-row class="form-group">
           <b-form class="w-100">
@@ -21,29 +22,19 @@
           </b-form>
         </b-row>
       </b-col>
+      <div class="border-right mx-2" style="width: 1px;" />
       <b-col>
-        <div class="ui vertical divider">
-          <i class="angle double right icon"></i>
-        </div>
-      </b-col>
-      <b-col class="mx-2">
         <b-row>
-          <h2 class="mx-auto" style="width: 180px;"><i class="car icon"></i> Drivers</h2>
+          <h2 class="mx-auto" style="width: 119px;"><i class="car icon"></i> Drivers</h2>
         </b-row>
         <b-row class="form-group">
           <b-form-input v-model="driverSearchInput" placeholder="Search" class="my-2"></b-form-input>
         </b-row>
       </b-col>
-
-      <b-col>
-        <div class="ui vertical divider">
-          <i class="angle double right icon"></i>
-        </div>
-      </b-col>
-
+      <div class="border-left mx-2" style="width: 1px;"/>
       <b-col>
         <b-row>
-          <h2 class="mx-auto" style="width: 180px">
+          <h2 class="mx-auto" style="width: 154px">
             <i class="child icon"></i>Passengers</h2>
         </b-row>
         <b-row>
@@ -112,14 +103,14 @@
                 <span class="font-weight-bold">Stops</span>
                 <table class="table">
                   <tbody v-for="stop in trip.stops">
-                      <tr>
-                        <th scope="row">Location</th>
-                        <td>{{stop.stopName}}</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">Price</th>
-                        <td>{{stop.price}}</td>
-                      </tr>
+                    <tr>
+                      <th scope="row">Location</th>
+                      <td>{{stop.stopName}}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Price</th>
+                      <td>{{stop.price}}</td>
+                    </tr>
                   </tbody>
                 </table>
               </b-collapse>
@@ -127,11 +118,12 @@
           </b-list-group>
         </b-row>
       </b-col>
-      <b-col>
+      <div class="border-right mx-2" style="width: 1px;" />
+      <!-- <b-col>
         <div class="ui vertical divider">
         </div>
-      </b-col>
-      <b-col class="mx-2">
+      </b-col> -->
+      <b-col>
         <b-row>
           <b-list-group class="w-100">
             <b-list-group-item class="text-center" v-b-toggle="'driverCollapse' + user.id" href="#" v-for="user in filteredDrivers" :key="user.id">
@@ -150,10 +142,11 @@
           </b-list-group>
         </b-row>
       </b-col>
-      <b-col>
+      <div class="border-right mx-2" style="width: 1px;" />
+      <!-- <b-col>
         <div class="ui vertical divider">
         </div>
-      </b-col>
+      </b-col> -->
       <b-col>
         <b-row>
           <b-list-group class="w-100">
